dba320408e9cf86012639c2205d59b7d694a58ac
Parada <paradafaii28@gmail.com>
2022-03-17 15:51:02 -0400

    create condition to checked if the score equl to zero show the word you lost the game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -25,12 +25,22 @@ document.querySelector('.check').addEventListener('click', function() {
     }else if (guess === secretNumber){
         document.querySelector('.message').textContent = '🎉 Correct Number!';
     }else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = '📈 Too high!';
-        score = score - 1;
-        document.querySelector('.score').textContent = score;
+        if (score > 1) {
+            document.querySelector('.message').textContent = '📈 Too high!';
+            score = score - 1;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '🎇 You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = '📉 Too low!';
-        score--;
-        document.querySelector('.score').textContent = score;
+        if (score > 1) {
+            document.querySelector('.message').textContent = '📉 Too low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '🎇 You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
