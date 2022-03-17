f2626c8eb925ed06862d5bc6b4587a052f451389
Parada <paradafaii28@gmail.com>
2022-03-17 15:31:57 -0400

    create condition to check the number is correct or higher or lower also set the variable to keep tracking the score

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,8 +12,9 @@
 // console.log(document.querySelector('.guess').value);
 
 // create random number
-const number = Math.trunc(Math.random()*20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -21,6 +22,15 @@ document.querySelector('.check').addEventListener('click', function() {
 
     if(!guess) {
         document.querySelector('.message').textContent = '⛔️ No Number!';
-
+    }else if (guess === secretNumber){
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+    }else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '📈 Too high!';
+        score = score - 1;
+        document.querySelector('.score').textContent = score;
+    }else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = '📉 Too low!';
+        score--;
+        document.querySelector('.score').textContent = score;
     }
-})
+});
