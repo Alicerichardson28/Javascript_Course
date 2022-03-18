fa99125280322af35e40aac3658856ccdc4130a5
Parada <paradafaii28@gmail.com>
2022-03-18 09:54:56 -0400

    style background with DOM by used style attributes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -20,10 +20,18 @@ document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
+    // When guess is no input
     if(!guess) {
         document.querySelector('.message').textContent = '⛔️ No Number!';
+
+    // When player wins
     }else if (guess === secretNumber){
         document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+        // change the color of the page
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+    // When guess is too high
     }else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = '📈 Too high!';
@@ -33,6 +41,7 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.message').textContent = '🎇 You lost the game!';
             document.querySelector('.score').textContent = 0;
         }
+    // When guess is too low
     }else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = '📉 Too low!';
