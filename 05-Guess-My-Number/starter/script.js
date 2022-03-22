1d39c6cbe87365337feaf1c6ecb4a32a737407db
Parada <paradafaii28@gmail.com>
2022-03-22 11:04:00 -0400

    refactoring code

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -16,18 +16,23 @@ let secretNumber = Math.trunc(Math.random()*20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
 
     // When guess is no input
     if(!guess) {
-        document.querySelector('.message').textContent = '⛔️ No Number!';
+        // document.querySelector('.message').textContent = '⛔️ No Number!';
+        displayMessage('⛔️ No Number!')
 
     // When player wins
     }else if (guess === secretNumber){
-        document.querySelector('.message').textContent = '🎉 Correct Number!';
+        // document.querySelector('.message').textContent = '🎉 Correct Number!';
+        displayMessage('🎉 Correct Number!')
         document.querySelector('.number').textContent = secretNumber;
         // change the color of the page
         document.querySelector('body').style.backgroundColor = '#60b347';
@@ -39,27 +44,41 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.highscore').textContent = highScore;
         }
         
-    // When guess is too high
-    }else if (guess > secretNumber) {
+    // when guess is wrong   
+    } else if(guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = '📈 Too high!';
+            // document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+            displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!')
             score = score - 1;
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.message').textContent = '🎇 You lost the game!';
-            document.querySelector('.score').textContent = 0;
-        }
-    // When guess is too low
-    }else if (guess < secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = '📉 Too low!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '🎇 You lost the game!';
+            // document.querySelector('.message').textContent = '🎇 You lost the game!';
+            displayMessage('🎇 You lost the game!')
             document.querySelector('.score').textContent = 0;
         }
     }
+
+    // // When guess is too high
+    // else if (guess > secretNumber) {
+    //     if (score > 1) {
+    //         document.querySelector('.message').textContent = '📈 Too high!';
+    //         score = score - 1;
+    //         document.querySelector('.score').textContent = score;
+    //     } else {
+    //         document.querySelector('.message').textContent = '🎇 You lost the game!';
+    //         document.querySelector('.score').textContent = 0;
+    //     }
+    // // When guess is too low
+    // }else if (guess < secretNumber) {
+    //     if (score > 1) {
+    //         document.querySelector('.message').textContent = '📉 Too low!';
+    //         score--;
+    //         document.querySelector('.score').textContent = score;
+    //     } else {
+    //         document.querySelector('.message').textContent = '🎇 You lost the game!';
+    //         document.querySelector('.score').textContent = 0;
+    //     }
+    // }
 });
 
 // Implement a game rest functionality, so that the player can make a new guess!
@@ -68,7 +87,8 @@ document.querySelector('.again').addEventListener('click', function() {
     score = 20;
     secretNumber = Math.trunc(Math.random()*20) + 1;
     
-    document.querySelector('.message').textContent = 'Start guessing...';
+    // document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...')
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = '?';
     // reset the value to empty string
