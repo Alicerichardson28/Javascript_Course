944cc5719577e9e5ca66ab3db13203cd2ef56024
Parada <paradafaii28@gmail.com>
2022-03-22 12:43:49 -0400

    refractoring duplicate code in to fucntion and passing into addEventlistener add a argument

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,20 +3,45 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 
-for(let i = 0; i < btnsShowModal.length; i++){
-    btnsShowModal[i].addEventListener('click', function() {
-        console.log('Button click');
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden'); 
 }
 
-    // add eventlistener to closeModal button
-    btnCloseModal.addEventListener('click', function() {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    });
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+//refractoring add eventlistener to showModal button
+for(let i = 0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', openModal);
+}   
+
+// for(let i = 0; i < btnsShowModal.length; i++){
+//     btnsShowModal[i].addEventListener('click', function() {
+//         modal.classList.remove('hidden');
+//         overlay.classList.remove('hidden'); 
+//     });
+// }   
+
+
+//refractoring add eventlistener to closeModal button
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal)
+
+// // add eventlistener to closeModal button
+// btnCloseModal.addEventListener('click', function() {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// });
+
+// overlay.addEventListener('click', function() {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// })
 
