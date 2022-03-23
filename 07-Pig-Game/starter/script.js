f299f08fbbf3f55ada971bd1f47ff824d30687be
Parada <paradafaii28@gmail.com>
2022-03-23 11:43:12 -0400

    used toggle to change UI background color after switch the player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -9,6 +11,7 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
 
 // Starting conditions
 score0El.textContent = 0;
@@ -43,5 +46,8 @@ btnRoll.addEventListener('click', function() {
         document.getElementById(`current--${activePlayer}`).textContent = 0;
         currentScore = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
+        // Change the UI background color
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 });
