a710557c1c520c83433d6f02b7ecb44ccac9643d
Parada <paradafaii28@gmail.com>
2022-03-23 11:35:47 -0400

    implement else statment switch to another player also reset the current score to 0

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -16,7 +16,9 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 // Set current score
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality 
 btnRoll.addEventListener('click', function() {
@@ -33,9 +35,13 @@ btnRoll.addEventListener('click', function() {
         // Add dice to current score
         // currentScore = currentScore + dice;
         currentScore += dice;
-        current0El.textContent = currentScore; // Change later
-         
+
+        // building id name
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // Switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
     }
 });
